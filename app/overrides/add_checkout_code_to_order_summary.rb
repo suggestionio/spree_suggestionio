7b50f2dbd5e4822c18a64e2d5c560ac8c6bad841
Richard Hart <richard@ur-ban.com>
2014-10-13 23:24:35 +0100

    Added main snippet

---
+++ app/overrides/add_checkout_code_to_order_summary.rb
@@ -0,0 +1,5 @@
+Deface::Override.new name: "add_snippet_to_footer",
+  virtual_path: "spree/shared/_footer",
+  insert_bottom: %{#footer[data-hook]},
+  original: 'c63cc7ea27083cc9cc69846109aab89f7a0fbe8a',
+  partial: "spree/shared/suggestionio"
